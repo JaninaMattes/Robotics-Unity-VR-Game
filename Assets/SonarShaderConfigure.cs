ed38f9c2ba23bdad0916bcef5e0f51d738764769
Janina Mattes <jm105@hdm-stuttgart.de>
2019-11-04 09:17:02 +0100

    Implementation sonar shader

---
+++ Assets/SonarShaderConfigure.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SonarShaderConfigure : MonoBehaviour
+{
+    // Reference to object material
+    public Material SonarMaterial;
+
+    private void Start()
+    {
+        foreach (Renderer rend in GetComponentsInChildren<Renderer>(true))
+        {
+            Texture mainTex = rend.material.mainTexture;
+            rend.material = SonarMaterial;
+            rend.material.mainTexture = mainTex;
+        }
+    }
+    
+}

8878c5cdce3df3de5df8b355f16d369421707b6d
Janina Mattes <jm105@hdm-stuttgart.de>
2019-11-08 21:14:51 +0100

    src: Sonar shader update

---
+++ Assets/Scripts/SonarSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarSpawner : MonoBehaviour
+{
+    public SonarState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // For debugging
+        //if (Input.GetMouseButtonDown(0))
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            var pos = SensorWorldPos();
+            // Takes mouse position and translates it into a ray
+            // which then can be projected into 3D space
+            var ray = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Overwrites the position of raycast with the following
+                state.SonarOrigin = hit.point;
+            }
+        }
+    }
+
+    Vector3 SensorWorldPos()
+    {
+        Vector3 pos = OVRManager.tracker.GetPose(0).position;
+        Debug.Log($"worldpos X: {pos.x}, Y: {pos.y}, Z: {pos.z}");
+        return pos;
+    }
+
+    Vector3 OnCollisionEnter(Collision col)
+    {
+        Vector3 pos = col.transform.position;
+        return pos;
+    }
+}
---
+++ Assets/Scripts/SonarState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarState : MonoBehaviour
+{
+    // Properties
+    public float speed;
+    public Material material;
+    private Vector4 sonarOrigin = Vector4.one;
+
+    // Setter and Getter
+    public Vector3 SonarOrigin { set { sonarOrigin = new Vector4(value.x, value.y, value.z, 0); } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If sonar ring exceedes one it gets reduced 
+        // so that colors are not inverted
+        sonarOrigin.w = Mathf.Min(sonarOrigin.w + (Time.deltaTime * speed), 1);
+        material.SetVector("_SonarOrigin", sonarOrigin);
+    }
+}

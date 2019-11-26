134c2764dd78d9c94349a30eff7d1246948a7575
Maurice <mf129@hdm-stuttgart.de>
2019-11-26 16:46:54 +0100

    Sonar Scripts updated
    Sonar Creator Script detects first collision of gameobject , the scripts are attached.
    
    Sonar State Script checks for collision state true and changes Shadervariable to the Vector of the collisionpoint stored in Sonar Creator Script.
    
    -> can be summarized in one script and programmed more efficiently

--- Assets/Scripts/SonarState.cs
+++ Assets/Scripts/SonarState.cs
@@ -7,7 +7,7 @@ public class SonarState : MonoBehaviour
     // Properties
     public float speed;
     public Material material;
-    private Vector4 sonarOrigin = Vector4.one;
+    public Vector4 sonarOrigin = Vector4.one;
 
     // Setter and Getter
     public Vector3 SonarOrigin { set { sonarOrigin = new Vector4(value.x, value.y, value.z, 0); } }
@@ -15,6 +15,10 @@ public class SonarState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.GetComponent<SonarCreator>().switchSonar)
+        {
+            sonarOrigin = gameObject.GetComponent<SonarCreator>().firstCollision;
+        }
         // If sonar ring exceedes one it gets reduced 
         // so that colors are not inverted
         sonarOrigin.w = Mathf.Min(sonarOrigin.w + (Time.deltaTime * speed), 1);

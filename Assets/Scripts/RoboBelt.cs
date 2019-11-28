e22d7d79bf1966c3689042bc167b0cd755d1642b
Janina Mattes <jm105@hdm-stuttgart.de>
2019-11-28 18:14:14 +0100

    src: Add pistol asset

---
+++ Assets/Scripts/RoboBelt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class RoboBelt : MonoBehaviour
+{
+    // Properties
+    public GameObject spawnObject;
+
+    private void OnTriggerStay(Collider col)
+    {
+        VRTK_InteractGrab grabbable = 
+            (col.gameObject.GetComponent<VRTK_InteractGrab>() ? 
+            col.gameObject.GetComponent<VRTK_InteractGrab>() : 
+            col.gameObject.GetComponentInParent<VRTK_InteractGrab>());
+
+        if (CanGrab(grabbable))
+        {
+            Debug.Log($"Spwaned the object {grabbable}");
+            GameObject spawned = Instantiate(spawnObject);
+            grabbable.GetComponent<VRTK_InteractTouch>().ForceTouch(spawnObject);
+            grabbable.AttemptGrab();
+        }
+    }
+
+    private bool CanGrab(VRTK_InteractGrab grab)
+    {
+        return true; 
+            //(grab && grab.GetGrabbedObject() == null && 
+            // grabpressed = when grab button is pressed to grab object
+            // TODO: Nachlesen in VRTK Dokumentation
+            // grab.gameObject.GetComponent<VRTK_ControllerEvents>().grabPressed);
+    }
+}

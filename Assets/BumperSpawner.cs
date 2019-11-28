6de86d3de5c8e32692168e0ddbe0ccbfbb32adf7
JaninaMattes <jm105@hdm-stuttgart.de>
2019-11-28 20:13:10 +0100

    src: Add bumper spawner skript

---
+++ Assets/BumperSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperSpawner : MonoBehaviour
+{
+    //Cube prefab
+    public GameObject bumper;
+    public GameObject player;
+    private float timer;
+
+    void onCollisionEnter(Collision col)
+    {
+        Vector3 contact = col.contacts[0].point;
+        Instantiate(player, contact, Quaternion.identity);
+    }
+}

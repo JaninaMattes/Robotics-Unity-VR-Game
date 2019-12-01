99c62d6c83d6421ff4f9cbe08801927e66a633b3
Yannik Schrempp <ys019@hdm-stuttgart.de>
2019-12-01 19:52:21 +0100

    Reactivated VRTK Simulator and changed SonarLaser to work with Interactable Objects "Use"

--- Assets/Scripts/SonarLaser.cs
+++ Assets/Scripts/SonarLaser.cs
@@ -1,25 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SonarLaser : MonoBehaviour
+﻿namespace VRTK.Examples
 {
-    private LineRenderer lr;
-    public Material material;
-    public Vector4 sonarOrigin = Vector4.one;
-    public float speed;
-    bool test = false;
-    
-    void Start()
-    {
-        lr = GetComponent<LineRenderer>();
-        lr.enabled = false;
-    }
-    
-    void Update()
+
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SonarLaser : MonoBehaviour
     {
 
-        if (Input.GetButtonDown("Fire1"))
+        public VRTK_InteractableObject laserPistol;
+
+
+        private LineRenderer lr;
+        public Material material;
+        public Vector4 sonarOrigin = Vector4.one;
+        public float speed;
+        bool test = false;
+
+        void Start()
+        {
+            lr = GetComponent<LineRenderer>();
+            lr.enabled = false;
+        }
+
+        protected virtual void OnEnable()
+        {
+            laserPistol = (laserPistol == null ? GetComponent<VRTK_InteractableObject>() : laserPistol);
+
+            if (laserPistol != null)
+            {
+                laserPistol.InteractableObjectUsed += InteractableObjectUsed;
+                laserPistol.InteractableObjectUnused += InteractableObjectUnused;
+            }
+
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (laserPistol != null)
+            {
+                laserPistol.InteractableObjectUsed -= InteractableObjectUsed;
+                laserPistol.InteractableObjectUnused -= InteractableObjectUnused;
+            }
+        }
+
+
+        protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
         {
             lr.enabled = true;
             StartCoroutine(WaitSonarShot());
@@ -30,20 +57,31 @@ public class SonarLaser : MonoBehaviour
             {
                 //if (hit.collider) Überprüfung ob getroffenes GameObject einen Collider hat / Auf Kollision überprüfen
                 //{
-                    lr.SetPosition(1, hit.point);
+                lr.SetPosition(1, hit.point);
                 sonarOrigin = hit.point;
                 //}
             }
             else lr.SetPosition(1, transform.forward * 5000);
         }
 
-        sonarOrigin.w = Mathf.Min(sonarOrigin.w + (Time.deltaTime * speed), 1);
-        material.SetVector("_SonarOrigin", sonarOrigin);
+        protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+        {
+
+        }
+
+
+
+        void Update()
+        {
+            sonarOrigin.w = Mathf.Min(sonarOrigin.w + (Time.deltaTime * speed), 1);
+            material.SetVector("_SonarOrigin", sonarOrigin);
+        }
+
+        IEnumerator WaitSonarShot()
+        {
+            yield return new WaitForSecondsRealtime(0.5f);
+            lr.enabled = false;
+        }
     }
 
-    IEnumerator WaitSonarShot()
-    {
-        yield return new WaitForSecondsRealtime(0.5f);
-        lr.enabled = false;
-    }
 }

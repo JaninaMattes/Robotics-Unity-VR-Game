5119ef04c0cf5cc7603fe59f9983190df8cdfa8c
Maurice <mf129@hdm-stuttgart.de>
2019-12-01 15:04:34 +0100

    SonarLaser Script added

---
+++ Assets/Scripts/SonarLaser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarLaser : MonoBehaviour
+{
+    private LineRenderer lr;
+    public Material material;
+    public Vector4 sonarOrigin = Vector4.one;
+    public float speed;
+    
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.enabled = false;
+    }
+    
+    void Update()
+    {
+        sonarOrigin.w = Mathf.Min(sonarOrigin.w + (Time.deltaTime * speed), 1);
+        //material.SetVector("_SonarOrigin", sonarOrigin);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            lr.enabled = true;
+            StartCoroutine(WaitSonarShot());
+        }
+
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            lr.SetPosition(1, hit.point);
+            sonarOrigin = hit.point;
+        }
+        else lr.SetPosition(1, transform.forward * 5000);
+    }
+
+    IEnumerator WaitSonarShot()
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+        lr.enabled = false;
+    }
+}

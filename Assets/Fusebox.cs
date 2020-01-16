c384c5879eeb50fe8510f8d37ebfa7c9b503df31
yyfryyy <ys019@hdm-stuttgart.de>
2020-01-16 23:21:32 +0100

    Added FuseboxModel + Interactions

---
+++ Assets/Fusebox.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fusebox : MonoBehaviour
+{
+
+    bool fuseStatus;
+    bool circuitClosed;
+
+    public Transform funkenLocation;
+
+    public GameObject Funken;
+
+    public GameObject Lever;
+
+    public AudioSource SoundEffekt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    
+    public void SetSicherungTrue()
+    {
+        fuseStatus = true;
+    }
+
+    public void SetSicherungFalse()
+    {
+        fuseStatus = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Is Triggered");
+        if (other.gameObject== Lever)
+        {
+            Debug.Log("Is Lever");
+            if (fuseStatus)
+            {
+                circuitClosed = true;
+            }
+            else
+            {
+                SoundEffekt.Play();
+                Instantiate(Funken, funkenLocation.transform.position, funkenLocation.transform.rotation);
+            }
+        }
+    }
+
+
+}

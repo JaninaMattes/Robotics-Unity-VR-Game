4f0a6a967b33135983a2c232ea4112aada6f4ff3
yyfryyy <ys019@hdm-stuttgart.de>
2020-01-22 17:58:52 +0100

    Raycast Flying ocjects works to change color

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+    public Material hit;
+    public Material hitandshot;
+    public Material notHit;
+
+    private bool _isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (_isHit)
+        {
+            GetComponent<Renderer>().material = hit;
+        }
+        else
+        {
+            GetComponent<Renderer>().material = notHit;
+        }
+
+        if(OVRInput.Get(OVRInput.Button.One))
+        {
+            GetComponent<Renderer>().material = hitandshot;
+        }
+        else
+        {
+            GetComponent<Renderer>().material = notHit;
+        }
+
+        _isHit = false;
+
+    }
+
+
+    public void BeenHit()
+    {
+        _isHit = true;
+    }
+}
--- Assets/Scripts/raycast_gunflyingobjects.cs
+++ Assets/Scripts/raycast_gunflyingobjects.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class raycast_gunflyingobjects : MonoBehaviour
 {
-    public float maxRayDistance = 300;
+    //public float maxRayDistance = 300; // alter Versuch Raycast
 
     // Start is called before the first frame update
     void Start()
@@ -15,16 +15,33 @@ public class raycast_gunflyingobjects : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Ray ray = new Ray(transform.position, Vector3.left);
         RaycastHit hit;
 
-        Debug.DrawLine(transform.position, transform.position + Vector3.left * maxRayDistance, Color.red);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 100f, Color.red);
+
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection (Vector3.up), out hit, Mathf.Infinity))
+        {
+            Debug.Log("getroffen");
+
+            hit.collider.gameObject.GetComponent<EnemyHit>().BeenHit();
+
+        }
+
+
+
+        //Alter Versuch Raycast
+        /*
+        Ray ray = new Ray(transform.position, Vector3.right);
+        RaycastHit hit;
+
+        Debug.DrawLine(transform.position, transform.position + Vector3.right * maxRayDistance, Color.red);
 
         if (Physics.Raycast(ray, out hit, maxRayDistance))
         {
             Debug.Log("Treffer");
-            Debug.DrawLine(transform.position, transform.position + Vector3.right * maxRayDistance, Color.blue);
+            Debug.DrawLine(hit.point,hit.point + Vector3.up * 5, Color.blue);
         }
-
+        */
     }
 }

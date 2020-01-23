8278d7eb609a6fb77fd00302a4294236408bdb22
Yannik Schrempp <yannikschrempp@web.de>
2020-01-23 13:11:59 +0100

    Added new Scanner-Gun Model, Lasergun Script & Laser Prefab

---
+++ Assets/LaserProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class LaserProjectile : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [HideInInspector]
+    public float speed=1;
+    [HideInInspector]
+    public GameObject explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.AddRelativeForce(Vector3.back * speed);  
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Scanner")
+        {
+            Hit();
+        }
+    }
+
+    void Hit()
+    {
+        Debug.Log("Boom Hit");
+        Instantiate(explosionPrefab, gameObject.transform.position-rb.velocity.normalized*0.2f, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Lasergun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lasergun : MonoBehaviour
+{
+
+    public GameObject laserBlastPrefab;
+
+    public Transform laserGunOrigin;
+
+    public int projectileSpeed = 200;
+
+    public GameObject explosionPrefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject blast = Instantiate(laserBlastPrefab, laserGunOrigin.position, gameObject.transform.rotation);
+        LaserProjectile projectileScript = blast.GetComponent<LaserProjectile>();
+        projectileScript.speed = projectileSpeed;
+        projectileScript.explosionPrefab = explosionPrefab;
+    }
+}

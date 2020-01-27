6d749c26e071871f22f33c1496fc02402376c21b
Maurice <mf129@hdm-stuttgart.de>
2020-01-27 02:06:00 +0100

    Audio methods script with explanation added + Assets folder "Audio"

---
+++ Assets/Audio/PlayAudioMethods.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Collection of most important methods regading the AudioSource component
+/// Unity Docs AudioSource -> https://docs.unity3d.com/ScriptReference/AudioSource.html
+/// </summary>
+
+public class PlayAudioMethods : MonoBehaviour
+{
+
+    [Header("Audio Source")]
+    public AudioSource audioSource;
+    [Header("Audio Clip")] // Audio clip needs to be played via audioSource. an audiosource contains an audioclip (example mp3)
+    public AudioClip audioClip;
+    [Header("Delay on Play")]
+    public float delayTime = 0;
+    [Header("Volume")]
+    public float volumeScale = 0.5f;
+    public float volume = 1f;
+    [Header("Play Audio Clip at Point in Worldspace")]
+    public Vector3 audioClipPosition = new Vector3();
+    [Header("Example Object to add an AudioSource via Script")]
+    public GameObject addAudioSource;
+
+    //Plays the audioclip of the audioSource
+    private void PlayAudioSource()
+    {
+        this.audioSource.Play();
+    }
+
+    //Plays the audioclip of the audioSource with a delay in seconds
+    private void PlayAudioSourceDelayed()
+    {
+        this.audioSource.PlayDelayed(this.delayTime);
+    }
+
+    //Plays an AudioClip, and scales the AudioSource volume by volumeScale.
+    private void PlayAudioSourceOneShot()
+    {
+        this.audioSource.PlayOneShot(this.audioClip, this.volumeScale);
+    }
+
+    //Pauses the audioclip of the audioSource 
+    private void PauseAudioSource()
+    {
+        this.audioSource.Pause();
+    }
+
+    //UnPauses the audioclip of the audioSource
+    private void UnPauseAudioSource()
+    {
+        this.audioSource.UnPause();
+    }
+
+    //Stops the audioclip of the audioSource 
+    private void StopAudioSource()
+    {
+        this.audioSource.Stop();
+    }
+
+    //Plays an AudioClip at a given position in world space and automatically disposes it once the clip has finished playing (no garbage)
+    private void PlayClipAtPosition()
+    {
+        AudioSource.PlayClipAtPoint(this.audioClip, this.audioClipPosition, this.volume);
+    }
+
+    //Add AudioSource to an example GameObject and setup an example audioClip and volume etc.
+    private void AddAudioSourceToGameObject()
+    {
+        this.addAudioSource.AddComponent<AudioSource>();
+        SetAudioClip(this.addAudioSource.GetComponent<AudioSource>(), this.audioClip);
+        SetVolume(addAudioSource, this.volume);
+    }
+
+    private void SetAudioClip(AudioSource audioSource, AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+    }
+
+    private void SetVolume(GameObject addAudioSource, float volume)
+    {
+        addAudioSource.GetComponent<AudioSource>().volume = volume;
+    }
+
+}

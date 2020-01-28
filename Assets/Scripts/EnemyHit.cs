f6bb871d2df061e53ea803cdb64d32c3e3c15268
yyfryyy <ys019@hdm-stuttgart.de>
2020-01-28 19:26:31 +0100

    Added ScoreText

--- Assets/Scripts/EnemyHit.cs
+++ Assets/Scripts/EnemyHit.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class EnemyHit : MonoBehaviour
 {
     public GameObject particleSystem;
 
-    public ScoringSystem scoringSystem;
+    ScoringSystem scoringSystem;
+
+    public GameObject score10Text;
+
+    public GameObject score25Text;
+
+    public GameObject score50Text;
+
+    public Material scoreMatBlau;
+
+    public Material scoreMatRot;
+
+   
+
+    GameObject activeCameraRig;
 
 
     public enum Relation
@@ -17,14 +32,32 @@ public class EnemyHit : MonoBehaviour
 
     public Relation relation;
 
+    public enum ScoreValue
+    {
+        _10 = 10,
+        _25 = 25,
+        _50 = 50,
+    }
+
+    public ScoreValue scoreValue;
+
+
+
+    //public int scoreValue = 10;
+
     
-
-    public int scoreValue = 10;
-
 
     private void Start()
     {
         scoringSystem = FindObjectOfType<ScoringSystem>();
+
+        Invoke("GetCameraRig", 0.05f);       
+        
+    }
+
+    void GetCameraRig()
+    {
+        activeCameraRig = GameObject.Find("CenterEyeAnchor");
     }
 
 
@@ -35,11 +68,72 @@ public class EnemyHit : MonoBehaviour
         if (relation == Relation.Enemy)
         {
             psMain.startColor = new Color(0.5f,0,0,1);
-            scoringSystem.SubtractLocalScore(scoreValue);
+            scoringSystem.SubtractLocalScore((sbyte)scoreValue);
+
+            GameObject scoreText = null;
+
+            switch(scoreValue)
+            {
+                case ScoreValue._10:
+                    //Spawn Text 10_Cube
+                    scoreText = Instantiate(score10Text, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case ScoreValue._25:
+                    //Spawn Text 25_Cube
+                    scoreText = Instantiate(score25Text, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case ScoreValue._50:
+                    //Spawn Text 50_Cube
+                    scoreText = Instantiate(score50Text, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+            }
+
+            //Vector3 dir = scoreText.transform.position - activeCameraRig.transform.position;
+            scoreText.transform.LookAt(activeCameraRig.transform);
+
+            Transform[] children = scoreText.GetComponentsInChildren<Transform>();
+            foreach (Transform child in children)
+            {
+                if (child.GetComponent<MeshRenderer>() != null)
+                {
+                    child.GetComponent<MeshRenderer>().material = scoreMatRot;
+                }
+    
+            }
+            
+
+            //for (int i = 0; i < scoreText.transform.childCount; i++)
+            //{
+            //    if (scoreText.transform.GetChild(i).GetComponent<MeshRenderer>() != null)
+            //    {
+            //        scoreText.transform.GetChild(i).GetComponent<MeshRenderer>().material = scoreMatRot;
+            //    }
+            //}
+       
         }
         else
         {
-            scoringSystem.AddLocalScore(scoreValue);
+            scoringSystem.AddLocalScore((sbyte)scoreValue);
+            GameObject scoreText = null;
+
+            switch (scoreValue)
+            {
+                case ScoreValue._10:
+                    //Spawn Text 10_Cube
+                    scoreText = Instantiate(score10Text, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case ScoreValue._25:
+                    //Spawn Text 25_Cube
+                    scoreText = Instantiate(score25Text, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+                case ScoreValue._50:
+                    //Spawn Text 50_Cube
+                    scoreText = Instantiate(score50Text, gameObject.transform.position, gameObject.transform.rotation);
+                    break;
+            }
+
+            scoreText.transform.LookAt(activeCameraRig.transform);
+
         }
 
         Destroy(gameObject);

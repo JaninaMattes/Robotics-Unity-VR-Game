7bb539a58f0d44fd9467c2afe06ba8b8b101f5eb
JaninaMattes <jm105@hdm-stuttgart.de>
2020-02-20 16:38:06 +0100

    Scoring System Kitchen + Confirm/ErrorSound
    Co-Authored-By: larakrauti <larakrauti@users.noreply.github.com>

--- Assets/Scripts/BucketList.cs
+++ Assets/Scripts/BucketList.cs
@@ -51,6 +51,24 @@ public class BucketList : MonoBehaviour
     protected IEnumerator delayCoroutine;
     protected GameObject[] checkListObjects;
 
+    public AudioSource correctObject;
+    public AudioSource IncorrectObject;
+
+    // Score Objects
+    ScoringSystem scoringSystem;
+
+    public GameObject score10;
+
+    public GameObject score25;
+
+    public GameObject scorePlus;
+
+    public GameObject scoreMinus;
+
+    GameObject activeCameraRig;
+
+    public int scoreValue;
+
 
 
     // Controller 
@@ -71,6 +89,9 @@ public class BucketList : MonoBehaviour
         SelectRandomObjects();
         // Create UI
         CreateDefaultUIText();
+
+        scoringSystem = FindObjectOfType<ScoringSystem>();
+        Invoke("GetCameraRig", 0.05f);
     }
 
     public void Update()
@@ -144,23 +165,65 @@ public class BucketList : MonoBehaviour
                 gameObj.GetComponent<VRTK_InteractableObject>().isGrabbable = false;
                 controller.AddToBucketList(gameObj);
                 controller.ResetMaterial(gameObj);
-                controller.SetPlayerScore(playerScore);
+                //controller.SetPlayerScore(playerScore);
                 //SetDefaultUIText(gameObj);
                 EnableUICheck(gameObj);
+                correctObject.Play();
+                scoreValue = 25;
+
+                //------Scoring ----------------------
+                scoringSystem.SubtractLocalScore((sbyte)scoreValue);
+
+                GameObject scoreText = null;
+
+                //Spawn Text 25_Cube
+                scoreText = Instantiate(score25, gameObject.transform.position, gameObject.transform.rotation);
+        
+
+                GameObject vorzeichen = Instantiate(scorePlus, gameObject.transform.position + new Vector3(1.5f, 0, 0), gameObject.transform.rotation);
+
+                vorzeichen.transform.parent = scoreText.transform;
+
+                //Vector3 dir = scoreText.transform.position - activeCameraRig.transform.position;
+                scoreText.transform.LookAt(activeCameraRig.transform);
+
+
+
+                Debug.Log("Korrektes Objekt: " + gameObj.tag);
+
             }
-            else if (!isFound(gameObj) && !controller.GetBucketObjects().Contains(gameObj))
+        else if (!isFound(gameObj) && !controller.GetBucketObjects().Contains(gameObj))
             {
                 // Set Gameobject back to it's original position
                 Vector3 position = controller.FindOriginalPos(gameObj);
                 delayCoroutine = DelayAndMove(gameObj, gameObj.transform.position, position, speed);
                 StartCoroutine(delayCoroutine);
+            
 
-                if (!coroutineCalled)
+            if (!coroutineCalled)
                 {
                     StartCoroutine("FlashColor");
-                    controller.ReducePlayerScore(playerScore);
-                }
-                else if (coroutineCalled)
+                //controller.ReducePlayerScore(playerScore);
+                Debug.Log("Inkorrektes Objekt: " + gameObj.tag);
+
+                IncorrectObject.Play();
+
+                //------Scoring ----------------------
+                scoreValue = 10;
+
+                scoringSystem.AddLocalScore((sbyte)scoreValue);
+                GameObject scoreText = null;
+
+                scoreText = Instantiate(score10, gameObject.transform.position, gameObject.transform.rotation);
+
+                GameObject vorzeichen = Instantiate(scoreMinus, gameObject.transform.position + new Vector3(1.5f, 0, 0), gameObject.transform.rotation);
+
+                vorzeichen.transform.parent = scoreText.transform;
+
+                scoreText.transform.LookAt(activeCameraRig.transform);
+
+            }
+            else if (coroutineCalled)
                 {
                     DisableDefaultUI();
                 }
@@ -286,6 +349,7 @@ public class BucketList : MonoBehaviour
                 checkedObjects[i].transform.GetChild(0).GetComponent<RawImage>().color = white;
                 ForceCanvasUpdate();
             }
+         
         }            
     }
 
@@ -344,5 +408,10 @@ public class BucketList : MonoBehaviour
         backgroundDefault.enabled = false;
         backgroundTransparent.enabled = true;
         backgroundDefault.enabled = true;
+    }
+
+    void GetCameraRig()
+    {
+        activeCameraRig = GameObject.Find("CenterEyeAnchor");
     }
 }

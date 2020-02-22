70d4e1065e794c505f5512ffbaa55bed60933640
Maurice <mf129@hdm-stuttgart.de>
2020-02-22 19:20:45 +0100

    CameraScreen UI

---
+++ Assets/BlinkLive.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkLive : MonoBehaviour
+{
+    public RawImage image;
+
+    void OnEnable()
+    {
+        StartBlinking();
+    }
+
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            switch (image.color.a.ToString())
+            {
+                case "0":
+                    image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+                    yield return new WaitForSeconds(0.9f);
+                    break;
+                case "1":
+                    image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+                    yield return new WaitForSeconds(0.6f);
+                    break;
+            }
+        }
+    }
+
+    void StartBlinking()
+    {
+        StopCoroutine("Blink");
+        //StopAllCoroutines();
+        StartCoroutine("Blink");
+    }
+
+    void StopBlinking()
+    {
+        StopAllCoroutines();
+    }
+
+}
+ 
+
+

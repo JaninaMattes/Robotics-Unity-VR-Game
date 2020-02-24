f49a672436208bed21afe1978cc1085dac279e9b
JaninaMattes <jm105@hdm-stuttgart.de>
2020-02-24 18:06:59 +0100

    kitchen stuff

--- Assets/Models/Scanner Gun/ExplosionPhysicsForce.cs
+++ Assets/Models/Scanner Gun/ExplosionPhysicsForce.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace UnityStandardAssets.Effects;
+namespace UnityStandardAssets.Effects
 {
     public class ExplosionPhysicsForce : MonoBehaviour
     {
